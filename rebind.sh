764cc63514fbf5dc2dc8cd0bf01b666251972916
Riccardo Cecolin <rikiji@rikiji.it>
2015-05-25 12:12:49 +0200

    Improve rebind script.

--- rebind.sh
+++ rebind.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
+if [[ $EUID -ne 0 ]]; then
+   echo "You are not root."
+   exit 1
+fi
+
 rmmod hid-generic
 rmmod naga
 insmod naga.ko
 
-for i in /sys/bus/usb/drivers/usbhid/1-*; do
+base=/sys/bus/usb/drivers/usbhid/
+
+check_id() {
+    for i in $1/*; do
+	if [[ $i =~ 1532:0040\.[0-9]{4} ]]; then
+	    return 0
+	fi
+    done
+    return 1
+}
+
+for i in $base/*; do
     name=$(basename $i)
-    echo $name
-    echo $name > /sys/bus/usb/drivers/usbhid/unbind 
-    echo $name > /sys/bus/usb/drivers/usbhid/bind 
+    if [[ $name =~ ^[0-9]\- ]]; then
+	if check_id $i; then
+	    echo "Naga: $name"
+	fi
+	echo $name > /sys/bus/usb/drivers/usbhid/unbind 
+	echo $name > /sys/bus/usb/drivers/usbhid/bind 
+    fi
 done
